bf564701bacfb451e5c135d8adfd6926388e21fc
Morales-coder10 <103393386+Morales-coder10@users.noreply.github.com>
2022-04-24 09:40:09 -0600

    Delete k.py

--- k.py
+++
@@ -1,7 +0,0 @@
-import tkinter as tk
-from tkintertable import TableCanvas, TableModel
-from tkintertable.Tables import TableCanvas
-from tkintertable.TableModels import TableModel
-
-model = TableModel()
-table = TableCanvas(frame, model=model)
